fb87f278ddc8d93299aa61d19b1673cc6305018f
Juan David Tapia Frias <juandavidtapia123@gmail.com>
2026-02-09 14:09:28 -0600

    LocalStorage[

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,24 @@ import './App.css'
 import Card from './Components/Card'
 import Footer from './Components/Footer'
 import Header from './Components/Header'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import { db } from './db/db'
 
 function App() {
-  const [cart, setCart] = useState([]);
+const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart');
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  };
+
+  const [cart, setCart] = useState(initialCart);
   const [data] = useState(db);
 
-  // Función para aumentar cantidad
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
+
+  //para aumentar cantidad
   function increaseQuantity(id) {
     const updatedCart = cart.map(item => {
       if (item.id === id) {
@@ -21,7 +31,7 @@ function App() {
     setCart(updatedCart);
   }
 
-  // Función para disminuir cantidad
+  //para disminuir cantidad
   function decreaseQuantity(id) {
     const updatedCart = cart.map(item => {
       if (item.id === id && item.quantity > 1) {
